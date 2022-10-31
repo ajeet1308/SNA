487d7d5c2fdd96e47212a220b799551b495f7b7d
Ajeet Kumar Jaiswal <66054508+ajeet1308@users.noreply.github.com>
2022-10-31 14:10:20 +0530

    first commit

---
+++ IMDB-Semantic-Sentiment-Analysis-main/scrapy.py
@@ -0,0 +1,80 @@
+#Import libraries
+import numpy as np
+import pandas as pd
+from scrapy.selector import Selector
+from selenium import webdriver 
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+from tqdm import tqdm
+import warnings
+warnings.filterwarnings("ignore")
+#Selenium Test
+driver = webdriver.Chrome('chromedriver.exe')
+url = 'https://www.imdb.com/title/tt0241527/reviews?ref_=tt_sa_3'
+time.sleep(1)
+driver.get(url)
+time.sleep(1)
+print(driver.title)
+time.sleep(1)
+body = driver.find_element(By.CSS_SELECTOR, 'body')
+body.send_keys(Keys.PAGE_DOWN)
+time.sleep(1)
+body.send_keys(Keys.PAGE_DOWN)
+time.sleep(1)
+body.send_keys(Keys.PAGE_DOWN)
+# Extracting review count and information from their
+rating_list = []
+review_date_list = []
+review_title_list = []
+author_list = []
+review_list = []
+review_url_list = []
+error_url_list = []
+error_msg_list = []
+reviews = driver.find_elements(By.CSS_SELECTOR, 'div.review-container')
+
+for d in tqdm(reviews):
+    try:
+        sel2 = Selector(text = d.get_attribute('innerHTML'))
+        try:
+            rating = sel2.css('.rating-other-user-rating span::text').extract_first()
+        except:
+            rating = np.NaN
+        try:
+            review = sel2.css('.text.show-more__control::text').extract_first()
+        except:
+            review = np.NaN
+        try:
+            review_date = sel2.css('.review-date::text').extract_first()
+        except:
+            review_date = np.NaN    
+        try:
+            author = sel2.css('.display-name-link a::text').extract_first()
+        except:
+            author = np.NaN    
+        try:
+            review_title = sel2.css('a.title::text').extract_first()
+        except:
+            review_title = np.NaN
+        try:
+            review_url = sel2.css('a.title::attr(href)').extract_first()
+        except:
+            review_url = np.NaN
+        rating_list.append(rating)
+        review_date_list.append(review_date)
+        review_title_list.append(review_title)
+        author_list.append(author)
+        review_list.append(review)
+        review_url_list.append(review_url)
+    except Exception as e:
+        error_url_list.append(url)
+        error_msg_list.append(e)
+review_df = pd.DataFrame({
+    'Review_Date':review_date_list,
+    'Author':author_list,
+    'Rating':rating_list,
+    'Review_Title':review_title_list,
+    'Review':review_list,
+    'Review_Url':review_url
+    })
